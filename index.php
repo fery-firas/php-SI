0479b0fca9c0ef44fd23da3b0823a8b58277752a
fery <firas@abv.bg>
2015-07-21 20:06:29 +0300

    dobaqne na html 02-practic

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+        <head>
+                      <title>   helo world  </title>
+
+       </head>
+
+   <body>
+             <h1> title</h1>
+              <div>
+			  text 
+			  </div>
+
+
+        </body>
+
+</html>
