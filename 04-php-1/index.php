5c8e559524603b4a76b9187bb85bebf30b4bc3a9
fery <firas@abv.bg>
2015-07-23 21:16:28 +0300

    index php imena i if()  else echo..

---
+++ 04-php-1/index.php
@@ -0,0 +1,41 @@
+<?php
+
+  $Name = "fery";
+  $Family = "zaiud";
+  
+					echo $Name;
+					
+					echo "<br/ >" ;
+					echo $Family;
+				echo "<br/ >" ;
+					echo  $Name.$Family;
+             		
+					echo "<br/ >" ;
+       
+
+
+	   $my_first_condition = 25;
+						
+					
+
+              $day_of_monht = 23;
+			  
+			  
+			  
+			  
+            if ($day_of_monht > 25 )
+                        {
+							
+						echo "Zaplatata nablijava";	
+							
+						}
+
+			else 
+					{
+	
+						echo "Daleche sme ot zaplata";
+	  
+					}
+  
+  
+?>
