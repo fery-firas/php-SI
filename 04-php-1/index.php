54c0c787b01abea32053f7534f75dfb261dfddd4
fery <firas@abv.bg>
2015-07-23 21:57:33 +0300

    krai sled  /*-+  matemat.

--- 04-php-1/index.php
+++ 04-php-1/index.php
@@ -1,41 +1,51 @@
 <?php
-
-  $Name = "fery";
-  $Family = "zaiud";
+$Name = "fery";
+$Family = "zaiud";
   
-					echo $Name;
-					
-					echo "<br/ >" ;
-					echo $Family;
-				echo "<br/ >" ;
-					echo  $Name.$Family;
-             		
-					echo "<br/ >" ;
-       
+echo $Name;
+echo "<br/ >" ;
+echo $Family;
+echo "<br/ >" ;
+
+echo  $Name.$Family;
+echo "<br/ >" ;
 
 
-	   $my_first_condition = 25;
-						
-					
-
-              $day_of_monht = 23;
+$day_of_monht = 40;
 			  
-			  
-			  
-			  
-            if ($day_of_monht > 25 )
-                        {
-							
-						echo "Zaplatata nablijava";	
-							
-						}
-
-			else 
-					{
+if ($day_of_monht < 25 || $day_of_monht == 25)
+{
 	
-						echo "Daleche sme ot zaplata";
-	  
-					}
+	echo "Zaplatata nablijava";	
+}
+else 
+{
+
+	echo "Daleche sme ot zaplata";
+
+}
+  echo "<br>";
   
+  $a = 2;
+  $b = 5;
+  $c = 6;
+  $d = 9;
+  $c = $a+$b;
+  $v = $c/$a;
+  $g = $d*$b;
+  
+	echo $c ;
+	echo "<br/ >" ;
+	
+	echo $v ;
+	echo "<br/ >" ;
+	echo $g ;
+	
+  
+  
+                  
+  
+  
+					
   
 ?>
