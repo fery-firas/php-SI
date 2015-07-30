05f6015bcfd82726d9baf3b76d13d21bf33684f8
fery <firas@abv.bg>
2015-07-30 03:34:02 +0300

    задача 1 домашно от if

---
+++ 04-php-1 -homework/1dom.php
@@ -0,0 +1,24 @@
+<?php
+
+$string = 15;
+
+if ($string == 15)
+{
+	echo"AZ SAM 15";
+}
+elseif ($string > 26) 
+ {
+	 
+	echo  "PO GOLQMO OT 26";
+	
+}
+elseif ($string <= 64) 
+ {
+	
+	 	echo '<span style="background-color:red;"> PO MALKO ILI RAVNO NA 64 </span>';
+	
+
+}
+
+?>
+
