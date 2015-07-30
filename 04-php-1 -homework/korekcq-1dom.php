8c315f5bbf844cbd72b3fd0ee99525ad89e1e592
fery <firas@abv.bg>
2015-07-31 00:09:06 +0300

    korekciq na domashnoto s if bez else if

---
+++ 04-php-1 -homework/korekcq-1dom.php
@@ -0,0 +1,24 @@
+<?php
+
+$string = 15;
+
+if ($string == 15)
+{
+	echo"AZ SAM 15";
+}
+if ($string > 26) 
+ {
+	 
+	echo  "PO GOLQMO OT 26";
+	
+}
+if ($string <= 64) 
+ {
+	
+	 	echo '<span style="background-color:red;"> <br />PO MALKO ILI RAVNO NA 64  </span>';
+	
+
+}
+
+?>
+
