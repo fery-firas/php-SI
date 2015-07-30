e2f94a52f238626b3752435a5c242653753f8b73
fery <firas@abv.bg>
2015-07-31 00:07:45 +0300

    zadacha 2 random

---
+++ 04-php-1 -homework/2random.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<style>
+
+body {
+	background-color= "#000000";
+}
+
+.randomcolor{
+	background-color: rgb(<?=rand(0, 255)?>, <?=rand(0, 255)?>, <?=rand(0, 255)?>);
+}
+</style>
+</head>
+<body>
+	<div class="randomcolor" style="width:200px;height:200px;">
+	<h1>Здравей CSS</h1>
+</body>
